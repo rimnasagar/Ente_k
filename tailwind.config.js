d5e6b3b53894e147db792bd3dbfbbb15e22acc6a
unknown <rimna.mk@gmail.com>
2024-02-01 18:04:36 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'montserrat': ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+    // other configurations...
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'sm': '640px', // Small screens and up
+          // ... other breakpoints
+        },
+      },
+    },
+    // ... other configurations
+  }
+  
